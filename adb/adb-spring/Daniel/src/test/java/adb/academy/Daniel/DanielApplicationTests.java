6a62f74350e69e54004523f88e40dc5383ecba7d
l14d35 <kujawa.dp@gmail.com>
2018-12-11 17:20:17 +0100

    In progress

---
+++ adb/adb-spring/Daniel/src/test/java/adb/academy/Daniel/DanielApplicationTests.java
@@ -0,0 +1,16 @@
+package adb.academy.Daniel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DanielApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
