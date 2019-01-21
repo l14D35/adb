4ca22f172804c3bce0bd78b749e2d5eb440b0169
l14d35 <kujawa.dp@gmail.com>
2019-01-21 15:57:35 +0100

    Watki piotrek

---
+++ java/watki/src/WatkiPiotrekTest.java
@@ -0,0 +1,26 @@
+public class WatkiPiotrekTest implements Runnable {
+    private static final int SIZE = 6;
+    private Thread w;
+
+    private WatkiPiotrekTest() {
+        w = new Thread(this);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        WatkiPiotrekTest[] p = new WatkiPiotrekTest[SIZE];
+        for (int i = 0; i < p.length; i++) {
+            p[i] = new WatkiPiotrekTest();
+            p[i].w.start();  // na rzecz watku tego obietku wywołuje metode run(poprzez metode start())
+            Thread.sleep(2000);
+        }
+
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Biegnie: " + Thread.currentThread().getId());
+
+    }
+
+}
