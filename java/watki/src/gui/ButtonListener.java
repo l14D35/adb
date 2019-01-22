dfbbd628d5fc9f88db151d24d403934ef862d7cf
l14d35 <kujawa.dp@gmail.com>
2019-01-22 10:40:07 +0100

    Button Listener - Easier

---
+++ java/watki/src/gui/ButtonListener.java
@@ -0,0 +1,35 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ButtonListener extends JFrame {
+    private JPanel buttonPanel;
+
+    private ButtonListener() {
+        this.setSize(300,200);
+
+        JButton redButton = new JButton("Red");
+        JButton greenButton = new JButton("Green");
+        JButton blueButton = new JButton("Blue");
+
+        buttonPanel = new JPanel();
+
+        buttonPanel.add(redButton);
+        buttonPanel.add(greenButton);
+        buttonPanel.add(blueButton);
+
+        add(buttonPanel);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        redButton.addActionListener(e -> buttonPanel.setBackground(Color.RED));
+        blueButton.addActionListener(e -> buttonPanel.setBackground(Color.BLUE));
+        greenButton.addActionListener(e -> buttonPanel.setBackground(Color.GREEN));
+    }
+
+    public static void main(String[] args) {
+        new ButtonListener();
+    }
+
+}
